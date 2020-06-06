24577371a5e0dcc59cdfe8a4409f4c0a38232bcc
sugu3 <teddy.picker.505@gmail.com>
2020-06-06 10:10:10 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// 関数
+
+// function 関数名(引数){
+//     // 処理
+//     return 戻り値;
+// }
+// 関数名(); //呼び出し
+
+function person(name,age,weight,height) {
+    document.write(`お名前は、${name}です。年齢は、${age}歳です。体重は、${weight}kgです。身長は、${height}cmです。<br>`);
+}
+// personの中はname = "高橋"
+person("高橋",20,60,175);
+person("鈴木",30,50,160);
+person("佐藤",40,55,170);
+
+
+// function sum(x,y) {
+//     document.onplaying(x * y);
+// }
+
+// sum(4 * 4);
